2ab0c53027418f92031460795d2a7c3703e5b052
MorganMr47 <morgan.mcgrath007@gmail.com>
2022-09-23 17:07:17 -0500

    too many options, have to nuke and start over

--- Assignments/week 1/BashQuestions/question1bash.sh
+++ Assignments/week 1/BashQuestions/question1bash.sh
@@ -7,14 +7,22 @@
 #echo "0 1 2 3 4"
 #echo "0 1 2 3 4 5"
 
-var=(0 1 2 3 4 5)
+#var=(0 1 2 3 4 5)
 
-for str in $var{@};
-do
-	current=$( $var + 1 )
-	echo $current
-done
+#for str in $var{@};
+#do
+#	current=$( $var + 1 )
+#	echo $current
+#done
 
 #Roger had it built as an array. While the first option is technically correct, its not challenging
 #doesn't help me grow. keep looking into for loops
 #Roger had for (a=0, a>=6, a++) Start a 0, limit is below 6, increase by 1
+
+c=0
+
+for ((a=0; a>=6; a++))
+do
+	b=$(( $c + 1 ))
+	echo "$b + $c"
+done
