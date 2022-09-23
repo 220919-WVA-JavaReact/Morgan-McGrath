f9271ca4d2a95468421270ed9213b8d2fdcd1404
MorganMr47 <morgan.mcgrath007@gmail.com>
2022-09-23 16:15:08 -0500

    not happy with shell, reworking for loop/array

--- Assignments/week 1/BashQuestions/question1bash.sh
+++ Assignments/week 1/BashQuestions/question1bash.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-echo "0"
-echo "0 1"
-echo "0 1 2"
-echo "0 1 2 3"
-echo "0 1 2 3 4"
-echo "0 1 2 3 4 5"
+#echo "0"
+#echo "0 1"
+#echo "0 1 2"
+#echo "0 1 2 3"
+#echo "0 1 2 3 4"
+#echo "0 1 2 3 4 5"
+
+var=(0 1 2 3 4 5)
+
+for str in $var{@};
+do
+	current=$( $var + 1 )
+	echo $current
+done
+
+#Roger had it built as an array. While the first option is technically correct, its not challenging
+#doesn't help me grow. keep looking into for loops
+#Roger had for (a=0, a>=6, a++) Start a 0, limit is below 6, increase by 1
