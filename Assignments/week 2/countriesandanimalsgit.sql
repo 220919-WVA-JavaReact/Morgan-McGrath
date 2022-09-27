3a10d7095f9b2062dd79d42d725f440794b9fb30
MorganMr47 <morgan.mcgrath007@gmail.com>
2022-09-27 13:56:30 -0500

    dbeaver exercise

---
+++ Assignments/week 2/countriesandanimalsgit.sql
@@ -0,0 +1,17 @@
+create table countries (
+	"name" varchar (25),
+	havenationalAnimal bool,
+	nationalAnimal varchar (25),
+	populationSizeinmill int,
+	climate varchar (20)
+);
+
+insert into countries values ('United States of America', true, 'Eagle', 330, 'mild');
+
+insert into countries values ('Republic of Indonesia', true, 'Komodo Dragon', 274, 'hot and humid');
+
+insert into countries values ('Republic of Ireland', true, 'Irish Hare', 5, 'mild, heavy rainfall');
+
+insert into countries values ('Fed Repub of Nigeria', true, 'Eagle', 206, 'tropical');
+
+insert into countries values ('Commonwealth of Australia', true, 'Kangaroo', 26, 'hot, varies');
